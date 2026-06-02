6970dcd00d2a87aba60230505719ca662f912ddc
EricChen34312 <85235002+EricChen34312@users.noreply.github.com>
2026-06-02 13:38:24 +0000

    add Inventory and Item classes

--- systems/Inventory.java
+++ systems/Inventory.java
@@ -1,5 +1,15 @@
 package systems;
 
+import java.util.ArrayList;
+
 public class Inventory {
+    private int size;
+    private ArrayList<Item> inv;
+
+    public Inventory(int size){
+        this.inv = new ArrayList<>();
+        this.size = size;
+    }
+
     
 }
