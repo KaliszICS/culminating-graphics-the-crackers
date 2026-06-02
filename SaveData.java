1995c3272abbabb9f4a583ff4c4d013f50632dad
JasonWu-ux <jasonwu544@gmail.com>
2026-06-02 13:43:56 +0000

    some of the save system

---
+++ SaveData.java
@@ -0,0 +1,15 @@
+public class SaveData {
+
+    // Player stats
+    public int playerHP;
+    public int playerMP;
+
+    // Boss stats at time of save (if not defeated)
+    public int plainsBossHP;
+    public int caveBossHP;
+    public int underworldBossHP;
+
+    // Map explore
+    public String currentMapArea; // "plains", "caves", "underworld"
+
+}
---
+++ SaveSystem.java
@@ -0,0 +1,35 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.io.Serializable;
+
+public class SaveSystem {
+    private String saveDirectory;
+    private String saveFilePath;
+
+    public SaveSystem() {
+        this.saveDirectory = System.getProperty(saveDirectory);
+        this.saveFilePath = 
+        createSaveDirectory();
+    }
+    public SaveSystem(String customPath) {
+        this.saveFilePath = customPath;
+        File file = new File(saveFilePath);
+        this.saveDirectory = file.getParent();
+        createSaveDirectory();
+    }
+
+    private void createSaveDirectory() {
+        File directory = new File(saveDirectory);
+        if (!directory.exists()) {
+            directory.mdirs();
+        }
+    }
+    //saveFile()
+    public SaveData saveFile() {
+        File file = new File(saveFilePath);
+        if (!file.exists()) {
+            System.out.println();
+            return null;
+        }
+    }
+}
