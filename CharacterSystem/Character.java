d12c1df4fb4508577510cb6921f48d9592344bee
JasonWu-ux <jasonwu544@gmail.com>
2026-06-04 13:47:22 +0000

    progess on Character system

---
+++ CharacterSystem/Character.java
@@ -0,0 +1,27 @@
+package CharacterSystem;
+
+public class Character {
+    private String name;
+    private int maxHP;
+    private int maxMP;
+    private int currentHP;
+    private int currentMP;
+    private int attackDMG;
+    private int speed;
+
+
+    public Character(String name, int maxHP, int maxMP, int currentHP, int currentMP, int attackDMG, int speed) {
+        this.name = name;
+        this.maxHP = maxHP;
+        this.maxMP = maxMP;
+        this.currentHP = currentHP;
+        this.currentMP = currentMP;
+        this.attackDMG = attackDMG;
+        this.speed = speed;
+        
+    }
+    
+    public void takeDamage(int amount) {
+        this.currentHP = Math.max(0, currentHP - amount);
+    }
+}
